0c2669eef413f98e06fb79ce241327d1280a37db
JeanBilheux <bilheuxjm@ornl.gov>
2015-05-04 15:25:39 -0400

    Added ignore file and finished up implementation and added full unit test set

--- produce_working_hours_test.py
+++ produce_working_hours_test.py
@@ -9,31 +9,76 @@ class testProduceWorkingHours(unittest.TestCase):
     def test_empty_input_string(self):
         obj = ProduceWorkingHours("")
         self.assertEqual(obj.nbrDaysWorked(), -1)
-    
-    def test_only_1_day_worked_from_any_string(self):
+        
+    def test_wrong_none_empty_string_format(self):
         obj = ProduceWorkingHours("any thing here")
-        self.assertEqual(obj.nbrDaysWorked(), 1)
-    
-    def test_only_1_day_worked_form_simple_8hours_day_string_correctly_formated(self):
+        self.assertEqual(obj.nbrDaysWorked(), 0)
+
+    def test_nbr_days_worked_for_simple_1_day_worked(self):
         obj = ProduceWorkingHours("11: 8h")
         self.assertEqual(obj.nbrDaysWorked(), 1)
         
-    def test_simle_2_days_worked_with_simple_8hours_day_string_correctly_formated(self):
+    def test_nbr_hours_worked_for_simple_1_day_worked(self):
+        obj = ProduceWorkingHours("11: 8h")
+        self.assertListEqual(obj.listHoursWorked(), ['8'])
+        
+    def test_nbr_days_worked_for_simple_2_days_worked(self):
         input_string = "20: 8h\n19: 8h"
         obj = ProduceWorkingHours(input_string)
         self.assertEqual(obj.nbrDaysWorked(), 2)
         
-    def test_simle_2_days_worked_plus_empty_string_with_simple_8hours_day_string_correctly_formated(self):
+    def test_nbr_days_worked_for_simple_2_days_worked_plus_empty_line(self):
         input_string = "20: 8h\n19: 8h\n"
         obj = ProduceWorkingHours(input_string)
         self.assertEqual(obj.nbrDaysWorked(), 2)
 
+    def test_nbr_hours_worked_for_simple_2_days_worked(self):
+        input_string = "20: 8h\n19: 9h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['8','9'])
     
+    def test_nbr_hours_worked_for_simple_2_days_worked_plus_empty_line(self):
+        input_string = "20: 8h\n19: 9h\n"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['8','9'])
+        
+    def test_nbr_hours_worked_for_complex_1_day_worked(self):
+        input_string = "5: 8:30->4:30"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['8.0'])
+    
+    def test_nbr_hours_worked_for_complex_1_day_worked_with_plus_half_h(self):
+        input_string = "5: 8:30->4:30 + .5h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['8.5'])
+    
+    def test_nbr_hours_worked_for_complex_1_day_worked_with_plus_one_and_halfh(self):
+        input_string = "5: 8:30->4:30 + 1.5h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['9.5'])
+
+    def test_nbr_hours_worked_for_complex_1_day_worked_with_minus_half_h(self):
+        input_string = "5: 8:30->4:30 - .5h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['7.5'])
+    
+    def test_nbr_hours_worked_for_complex_1_day_worked_with_minus_one_and_halfh(self):
+        input_string = "5: 8:30->4:30 - 1.5h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['6.5'])
+
+    def test_nbr_hours_worked_for_complex_1_day_worked_with_plus_and_minus_h(self):
+        input_string = "5: 8:30->4:30 - .5h + 1h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['8.5'])
+
+    def test_nbr_hours_worked_for_complex_2_days_worked_with_plus_and_minus_h(self):
+        input_string = "5: 8:30->4:30 - .5h + 1h\n 4: 8:30->5:30 - .5h + .5h"
+        obj = ProduceWorkingHours(input_string)
+        self.assertListEqual(obj.listHoursWorked(), ['8.5','9.0'])
 
 if __name__ == "__main__":
     unittest.main()
-    
-    
     
 # to run test
 # >> python produce_working_hours_test.py -v
--- runMe.py
+++ runMe.py
@@ -1,57 +1,58 @@
 from PyQt4 import QtGui
 import sys
+import re
 from main_window import *
 
+
 class FromEvernoteToTimeSaver(QtGui.QMainWindow):
-    
+
+    list_day_ui = []
+
+
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("From Evernote to Time Saver")
         self.ui.pushButton.setFocus()
+
+        self.list_day_ui = [self.ui.label_monday,
+                            self.ui.label_tuesday,
+                            self.ui.label_wednesday,
+                            self.ui.label_thursday,
+                            self.ui.label_friday,
+                            self.ui.label_saturday,
+                            self.ui.label_sunday]
         
     def convert(self):
         text_to_convert = self.ui.textEdit.toPlainText()
         o_working_hours = ProduceWorkingHours(text_to_convert)
         nbr_days_worked = o_working_hours.nbrDaysWorked()
-        list_days_worked = o_working_hours.listHoursWorked()
+        list_hours_worked = o_working_hours.listHoursWorked()
+        revert_list_hours_worked = self.revertList(list_hours_worked)
         
-        #result = text_to_convert.split("\n")
-        #week_working_hours = []
-        #for row in result:
-            #nbr_hours = self.retrieveNbrHoursWorkThatDay(row)
-            #week_working_hours.append(nbr_hours)
-        #print week_working_hours
-        
-        #nbr_days_worked = len(week_working_hours)
-        #for day_index in range(nbr_days_worked, 0, -1):
-            #pass
-            
-    #def retrieveNbrHoursWorkedThatDay(self, row):
-        #sep_date_hours = row.split(":")
-        #if len(sep_date_hours) == 1:
-            #return -1
-        
-        #hours_part = sep_date_hours[1:].join(":")
-        #full_day = hours_part.split("->")
-        #if len(full_day) == 1: #ex: 8h
-            #hours = full_day[0].split("h") # remove hours ('h') symbol
-            #return str(hours[0]).strip()
-        
-        #start_day_at = full_day[0]
-        #second_part_of_day = full_day[1].split("-")
-        
-        #return -1
+        self.populateLabels(revert_list_hours_worked)
 
+    def populateLabels(self, list_hours_worked):
+        for idx, hours_worked in enumerate(list_hours_worked):
+            label_ui = self.list_day_ui[idx].setText("%.1f"%(float(hours_worked)))
 
+    def revertList(self, list_hours_worked):
+        return list_hours_worked[::-1]
+        
 class ProduceWorkingHours(object):
     
     input_string = ''
     nbr_days_worked = -1
     list_day_worked = []
+    list_hours_worked = []
     
     def __init__(self, string_hours):
+        self.input_string = ''
+        self.nbr_days_worked = -1
+        self.list_day_worked = []
+        self.list_hours_worked = []
+
         self.input_string = string_hours
                 
         self.splitByDay()
@@ -59,27 +60,88 @@ class ProduceWorkingHours(object):
     def splitByDay(self):
         
         # if no argument passed
-        _input_string = self.input_string
+        _input_string = str(self.input_string)
         if _input_string.strip() == "":
             return
         
-        list_day_worked = _input_string.split("\n")
+        _list_day_worked = _input_string.split("\n")
 
         # get real number of days worked (remove empty lines)
         index_nbr_day = 0
-        for day in list_day_worked:
+        for day in _list_day_worked:
             if day.strip() != "":
                 index_nbr_day += 1
+                self.list_day_worked.append(day)
         self.nbr_days_worked = index_nbr_day
         
+        self.getListHoursWorked()
         
+    def getListHoursWorked(self):
+        _list_day_worked = self.list_day_worked
+        for _day in _list_day_worked:
+            set_date_from_second_part = _day.split(":")
+            
+            if len(set_date_from_second_part) == 1:
+                self.nbr_days_worked -= 1
+                continue
+            
+            if len(set_date_from_second_part) == 2:
+                hours_worked = self.calculateNumberOfHoursStraigthHours(set_date_from_second_part[1])
+                self.list_hours_worked.append(hours_worked)
+                continue
+            
+            second_part = ":".join(set_date_from_second_part[1:])
+            [start_day, end_day, minus_hours, plus_hours] = self.isolateStringParts(second_part)
+            hours_worked = self.calculateNumberOfHoursWorked([start_day, end_day, minus_hours, plus_hours])
+            self.list_hours_worked.append(hours_worked)
+    
+    def isolateStringParts(self, second_part):
+        second_part = second_part.replace(" ", "")
+        regular_expression = r"(\d+):(\d+)->(\d+):(\d+)(-\d?.?\d+)?h?(\+\d?.?\d?)?h?"
+        result_re = re.match(regular_expression, second_part)
+        
+        start_hour = int(result_re.group(1))
+        start_minute = float(result_re.group(2))/60.
+        start_hour_mn = float(start_hour) + start_minute
 
+        end_hour = int(result_re.group(3))
+        end_minute = float(result_re.group(4))/60.
+        end_hour_mn = float(end_hour) + end_minute
+        
+        minus_hour_str = result_re.group(5)
+        minus_hour = 0
+        if minus_hour_str:
+            split_by_neg_sign = minus_hour_str.split("-")
+            split_by_h = split_by_neg_sign[1].split("h")
+            minus_hour = float(split_by_h[0])
+        
+        plus_hour_str = result_re.group(6)
+        plus_hour = 0
+        if plus_hour_str: 
+            split_by_plus_sign= plus_hour_str.split("+")
+            split_by_h = split_by_plus_sign[1].split("h")
+            plus_hour = float(split_by_h[0])
+
+        return [start_hour_mn, end_hour_mn, minus_hour, plus_hour]
+
+    def calculateNumberOfHoursStraigthHours(self, second_part):
+        remove_h = second_part.split("h")
+        return remove_h[0].strip()
+    
+    def calculateNumberOfHoursWorked(self, start_end_minus_plus_list):
+        start_hour_mn = start_end_minus_plus_list[0]
+        end_hour_mn = 12+start_end_minus_plus_list[1]
+        minus_hour = start_end_minus_plus_list[2]
+        plus_hour = start_end_minus_plus_list[3]
+
+        total_hours = end_hour_mn - start_hour_mn - minus_hour + plus_hour
+        return str(total_hours)
     
     def nbrDaysWorked(self):
         return self.nbr_days_worked
         
     def listHoursWorked(self):
-        return []
+        return self.list_hours_worked
 
 
 if __name__ == "__main__":
